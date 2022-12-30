01538a2f6b2747c26946026cb937ce56f1426b4c
zenit1925 <kostia-85-85@yandex.ru>
2022-12-30 19:48:21 +0300

    Task 2.3.1 version1 30/12/2023

---
+++ PP_2_2_2_spring-mvc-master/src/main/java/web/dao/UserDaoImp.java
@@ -0,0 +1,47 @@
+package web.dao;
+
+import org.springframework.stereotype.Repository;
+import web.model.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    protected EntityManager getEntityManager() {
+        return this.entityManager;
+    }
+
+    @Override
+    public void addUser(User user) {
+        getEntityManager().persist(user);
+    }
+
+    @Override
+    public void removeUserById(Long id) {
+        User user = entityManager.find(User.class, id);
+        if (user != null) {
+            entityManager.remove(user);
+        }
+    }
+
+    @Override
+    public void editUser(User user) {
+        getEntityManager().merge(user);
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return getEntityManager().find(User.class, id);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return entityManager.createQuery("SELECT user FROM User user", User.class).getResultList();
+    }
+}
